fd772e9f20fd67e0251103bd73b69fdd42f34d48
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-29 00:05:58 -0700

    initial commit

---
+++ PhoneBook.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+class Phonebook
+{
+  public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+  static void Main()
+  {
+  Console.WriteLine("MAIN MENU");
+  Console.WriteLine("Would you like to add a person to your phone book? ['Y' for yes, 'Enter' for no]");
+  string answer = Console.ReadLine();
+  if (answer == "Y" || answer == "y")
+  {
+    AddContact();
+  }
+  else
+  {
+    Console.WriteLine("Would you like to look up a number in your phone book? ['Y' for yes, 'Enter' for no]");
+    string lookUpAnswer = Console.ReadLine();
+    if (lookUpAnswer == "Y" || lookUpAnswer == "y")
+    {
+      LookUpContact();
+    }
+    else
+    {
+      Console.WriteLine("Are you finished with this program? ['Y' for yes, 'Enter' for no]");
+      string finishedAnswer = Console.ReadLine();
+      if (finishedAnswer == "Y" || finishedAnswer == "y")
+      {
+        Console.WriteLine("Goodbye.");
+      }
+      else
+      {
+        Main();
+      }
+    }
+  }
+}
+}
